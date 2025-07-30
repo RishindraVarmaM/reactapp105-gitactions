530e5c1caf020d7a6902b210d3cb143a82763421
Rishi <2300031875@gmail.com>
2025-07-30 10:17:11 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+function App(){
+  return (
+    <div className="container">
+    <h1>Welcome to React Project - S105</h1>
+    <p>This is a simple homepge deployed on GitHub Pages using Vite.
+    </p>
+    </div>
+
+);
+}
+
+export default App;
